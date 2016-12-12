1d48c38efc942067cb1c960ba68c9ac55d8e0444
marvels <lzy@luozy.cn>
2016-12-12 18:25:23 +0800

    2016.12.12 18:25 changed

--- app/src/main/java/cn/luozy/signin/signin_student/SpeakerRegisterActivity.java
+++ app/src/main/java/cn/luozy/signin/signin_student/SpeakerRegisterActivity.java
@@ -1,25 +1,19 @@
 package cn.luozy.signin.signin_student;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.util.Log;
-import android.widget.EditText;
-import android.widget.RadioGroup;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.iflytek.cloud.ErrorCode;
-import com.iflytek.cloud.InitListener;
 import com.iflytek.cloud.SpeakerVerifier;
 import com.iflytek.cloud.SpeechConstant;
 import com.iflytek.cloud.SpeechError;
 import com.iflytek.cloud.SpeechListener;
-import com.iflytek.cloud.SpeechUtility;
 import com.iflytek.cloud.VerifierListener;
 import com.iflytek.cloud.VerifierResult;
 
@@ -35,16 +29,13 @@ public class SpeakerRegisterActivity extends AppCompatActivity {
 
     private TextView textView_title;
     private TextView textView_speaker_password;
-    private TextView textView_message;
+    private TextView textView_tips;
+    private Button button_start;
 
 
     private SpeakerVerifier mVerifier;
-    // 声纹AuthId，用户在云平台的身份标识，也是声纹模型的标识
-    // 请使用英文字母或者字母和数字的组合，勿使用中文字符
     private String mAuthId;
-    // 数字声纹密码
     private String mNumPwd;
-    // 数字声纹密码段，默认有5段
     private String[] mNumPwdSegs;
 
     private Toast mToast;
@@ -59,7 +50,8 @@ public class SpeakerRegisterActivity extends AppCompatActivity {
 
         textView_title = (TextView) findViewById(R.id.textView_title);
         textView_speaker_password = (TextView) findViewById(R.id.textView_password);
-        textView_message = (TextView) findViewById(R.id.textView_tips);
+        textView_tips = (TextView) findViewById(R.id.textView_tips);
+        button_start = (Button) findViewById(R.id.button_start);
 
         mToast = Toast.makeText(SpeakerRegisterActivity.this, "", Toast.LENGTH_SHORT);
 
@@ -67,7 +59,14 @@ public class SpeakerRegisterActivity extends AppCompatActivity {
         student_token = getIntent().getStringExtra("student_token");
         mAuthId = "student_" + student_id;
 
-        startGetPasswordList();
+        mVerifier = SpeakerVerifier.createVerifier(SpeakerRegisterActivity.this, null);
+        button_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mVerifier.cancel();
+                startGetPasswordList();
+            }
+        });
     }
 
     private void speakerComplete() {
@@ -79,7 +78,6 @@ public class SpeakerRegisterActivity extends AppCompatActivity {
     }
 
     private void startGetPasswordList() {
-        mVerifier = SpeakerVerifier.createVerifier(SpeakerRegisterActivity.this, null);
         mVerifier.setParameter(SpeechConstant.ISV_PWDT, "" + mPwdType);
         mVerifier.getPasswordList(mPwdListenter);
     }
@@ -91,14 +89,11 @@ public class SpeakerRegisterActivity extends AppCompatActivity {
 
         mVerifier.setParameter(SpeechConstant.ISV_PWD, mNumPwd);
         textView_speaker_password.setText(mNumPwdSegs[0]);
-        textView_message.setText("训练 第" + 1 + "遍，剩余4遍");
+        textView_tips.setText("训练 第" + 1 + "遍，剩余4遍");
 
         mVerifier.setParameter(SpeechConstant.AUTH_ID, mAuthId);
-        // 设置业务类型为注册
         mVerifier.setParameter(SpeechConstant.ISV_SST, "train");
-        // 设置声纹密码类型
         mVerifier.setParameter(SpeechConstant.ISV_PWDT, "" + mPwdType);
-        // 开始注册
         mVerifier.startListening(mRegisterListener);
     }
 
@@ -107,29 +102,14 @@ public class SpeakerRegisterActivity extends AppCompatActivity {
         mVerifier.setParameter(SpeechConstant.ISV_AUDIO_PATH,
                 Environment.getExternalStorageDirectory().getAbsolutePath() + "/msc/verify.pcm");
         mVerifier = SpeakerVerifier.getVerifier();
-        // 设置业务类型为验证
         mVerifier.setParameter(SpeechConstant.ISV_SST, "verify");
-
-        // 数字密码注册需要传入密码
         String verifyPwd = mVerifier.generatePassword(8);
         mVerifier.setParameter(SpeechConstant.ISV_PWD, verifyPwd);
         textView_speaker_password.setText(verifyPwd);
-
         mVerifier.setParameter(SpeechConstant.AUTH_ID, mAuthId);
         mVerifier.setParameter(SpeechConstant.ISV_PWDT, "" + mPwdType);
-        // 开始验证
         mVerifier.startListening(mVerifyListener);
     }
-
-    private void performModelOperation(String operation, SpeechListener listener) {
-        // 清空参数
-        mVerifier.setParameter(SpeechConstant.PARAMS, null);
-        mVerifier.setParameter(SpeechConstant.ISV_PWDT, "" + mPwdType);
-
-        // 设置auth_id，不能设置为空
-        mVerifier.sendRequest(operation, mAuthId, listener);
-    }
-
 
     private SpeechListener mPwdListenter = new SpeechListener() {
         @Override
@@ -147,7 +127,6 @@ public class SpeakerRegisterActivity extends AppCompatActivity {
                         if (!object.has("num_pwd")) {
                             return;
                         }
-
                         JSONArray pwdArray = object.optJSONArray("num_pwd");
                         numberString.append(pwdArray.get(0));
                         for (int i = 1; i < pwdArray.length(); i++) {
@@ -158,7 +137,7 @@ public class SpeakerRegisterActivity extends AppCompatActivity {
                     }
                     mNumPwd = numberString.toString();
                     mNumPwdSegs = mNumPwd.split("-");
-                    performModelOperation("que", mModelOperationListener);
+                    startVerify();
                     break;
                 default:
                     break;
@@ -169,42 +148,6 @@ public class SpeakerRegisterActivity extends AppCompatActivity {
         public void onCompleted(SpeechError error) {
             if (null != error && ErrorCode.SUCCESS != error.getErrorCode()) {
                 showTip("获取失败：" + error.getErrorCode());
-            }
-        }
-    };
-
-    private SpeechListener mModelOperationListener = new SpeechListener() {
-
-        @Override
-        public void onEvent(int eventType, Bundle params) {
-        }
-
-        @Override
-        public void onBufferReceived(byte[] buffer) {
-            String result = new String(buffer);
-            try {
-                JSONObject object = new JSONObject(result);
-                String cmd = object.getString("cmd");
-                int ret = object.getInt("ret");
-
-                if ("que".equals(cmd)) {
-                    if (ret == ErrorCode.SUCCESS) {
-                        textView_title.setText("开始验证声纹");
-                        startVerify();
-                    } else if (ret == ErrorCode.MSP_ERROR_FAIL) {
-                        textView_title.setText("开始注册声纹");
-                        startRegister();
-                    }
-                }
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-        }
-
-        @Override
-        public void onCompleted(SpeechError error) {
-            if (null != error && ErrorCode.SUCCESS != error.getErrorCode()) {
-                showTip("操作失败：" + error.getPlainDescription(true));
             }
         }
     };
@@ -222,53 +165,42 @@ public class SpeakerRegisterActivity extends AppCompatActivity {
                 switch (result.err) {
                     case VerifierResult.MSS_ERROR_IVP_GENERAL:
                         showTip("内核异常");
-                        // mShowMsgTextView.setText("内核异常");
                         break;
                     case VerifierResult.MSS_ERROR_IVP_EXTRA_RGN_SOPPORT:
                         showTip("训练达到最大次数");
-                        //  mShowRegFbkTextView.setText("训练达到最大次数");
                         break;
                     case VerifierResult.MSS_ERROR_IVP_TRUNCATED:
                         showTip("出现截幅");
-                        //  mShowRegFbkTextView.setText("出现截幅");
                         break;
                     case VerifierResult.MSS_ERROR_IVP_MUCH_NOISE:
                         showTip("太多噪音");
-                        //  mShowRegFbkTextView.setText("太多噪音");
                         break;
                     case VerifierResult.MSS_ERROR_IVP_UTTER_TOO_SHORT:
                         showTip("录音太短");
-                        //  mShowRegFbkTextView.setText("录音太短");
                         break;
                     case VerifierResult.MSS_ERROR_IVP_TEXT_NOT_MATCH:
                         showTip("训练失败，您所读的文本不一致");
-                        //  mShowRegFbkTextView.setText("训练失败，您所读的文本不一致");
                         break;
                     case VerifierResult.MSS_ERROR_IVP_TOO_LOW:
                         showTip("音量太低");
-                        // mShowRegFbkTextView.setText("音量太低");
                         break;
                     case VerifierResult.MSS_ERROR_IVP_NO_ENOUGH_AUDIO:
                         showTip("音频长达不到自由说的要求");
-                        // mShowMsgTextView.setText("音频长达不到自由说的要求");
                     default:
-                        // mShowRegFbkTextView.setText("");
-                        //showTip("训练达到最大次数");
                         break;
                 }
 
                 if (result.suc == result.rgn) {
-                    speakerComplete();
                     showTip("注册成功");
+                    speakerComplete();
                     //mResultEditText.setText("您的数字密码声纹ID：\n" + result.vid);
                 } else {
                     int nowTimes = result.suc + 1;
                     int leftTimes = result.rgn - nowTimes;
                     textView_speaker_password.setText(mNumPwdSegs[nowTimes - 1]);
-                    textView_message.setText("训练 第" + nowTimes + "遍，剩余" + leftTimes + "遍");
+                    textView_tips.setText("训练 第" + nowTimes + "遍，剩余" + leftTimes + "遍");
                 }
             } else {
-                // mShowMsgTextView.setText("注册失败，请重新开始。");
                 showTip("注册失败，请重新开始。");
             }
         }
@@ -276,11 +208,6 @@ public class SpeakerRegisterActivity extends AppCompatActivity {
         // 保留方法，暂不用
         @Override
         public void onEvent(int eventType, int arg1, int arg2, Bundle arg3) {
-            // 以下代码用于获取与云端的会话id，当业务出错时将会话id提供给技术支持人员，可用于查询会话日志，定位出错原因
-            //	if (SpeechEvent.EVENT_SESSION_ID == eventType) {
-            //		String sid = obj.getString(SpeechEvent.KEY_EVENT_SESSION_ID);
-            //		Log.d(TAG, "session id =" + sid);
-            //	}
         }
 
         @Override
@@ -313,42 +240,31 @@ public class SpeakerRegisterActivity extends AppCompatActivity {
         @Override
         public void onResult(VerifierResult result) {
             if (result.ret == 0) {
-                speakerComplete();
                 showTip("验证通过");
-                // 验证通过
-                //  mShowMsgTextView.setText();
+                speakerComplete();
             } else {
-                // 验证不通过
                 switch (result.err) {
                     case VerifierResult.MSS_ERROR_IVP_GENERAL:
                         showTip("内核异常");
-                        // mShowMsgTextView.setText("内核异常");
                         break;
                     case VerifierResult.MSS_ERROR_IVP_TRUNCATED:
                         showTip("出现截幅");
-                        //  mShowRegFbkTextView.setText("出现截幅");
                         break;
                     case VerifierResult.MSS_ERROR_IVP_MUCH_NOISE:
                         showTip("太多噪音");
-                        //  mShowRegFbkTextView.setText("太多噪音");
                         break;
                     case VerifierResult.MSS_ERROR_IVP_UTTER_TOO_SHORT:
                         showTip("录音太短");
-                        //  mShowRegFbkTextView.setText("录音太短");
                         break;
                     case VerifierResult.MSS_ERROR_IVP_TEXT_NOT_MATCH:
                         showTip("训练失败，您所读的文本不一致");
-                        //  mShowRegFbkTextView.setText("训练失败，您所读的文本不一致");
                         break;
                     case VerifierResult.MSS_ERROR_IVP_TOO_LOW:
                         showTip("音量太低");
-                        // mShowRegFbkTextView.setText("音量太低");
                         break;
                     case VerifierResult.MSS_ERROR_IVP_NO_ENOUGH_AUDIO:
                         showTip("音频长达不到自由说的要求");
-                        // mShowMsgTextView.setText("音频长达不到自由说的要求");
                     default:
-                        //mShowMsgTextView.setText("验证不通过");
                         break;
                 }
             }
@@ -357,11 +273,6 @@ public class SpeakerRegisterActivity extends AppCompatActivity {
         // 保留方法，暂不用
         @Override
         public void onEvent(int eventType, int arg1, int arg2, Bundle arg3) {
-            // 以下代码用于获取与云端的会话id，当业务出错时将会话id提供给技术支持人员，可用于查询会话日志，定位出错原因
-            //	if (SpeechEvent.EVENT_SESSION_ID == eventType) {
-            //		String sid = obj.getString(SpeechEvent.KEY_EVENT_SESSION_ID);
-            //		Log.d(TAG, "session id =" + sid);
-            //	}
         }
 
         @Override
@@ -369,9 +280,9 @@ public class SpeakerRegisterActivity extends AppCompatActivity {
 
             switch (error.getErrorCode()) {
                 case ErrorCode.MSP_ERROR_NOT_FOUND:
-                    //  mShowMsgTextView.setText("模型不存在，请先注册");
+                    textView_title.setText("注册声纹模型");
+                    startRegister();
                     break;
-
                 default:
                     showTip("onError Code：" + error.getPlainDescription(true));
                     break;
@@ -380,13 +291,11 @@ public class SpeakerRegisterActivity extends AppCompatActivity {
 
         @Override
         public void onEndOfSpeech() {
-            // 此回调表示：检测到了语音的尾端点，已经进入识别过程，不再接受语音输入
             showTip("结束说话");
         }
 
         @Override
         public void onBeginOfSpeech() {
-            // 此回调表示：sdk内部录音机已经准备好了，用户可以开始语音输入
             showTip("开始说话");
         }
     };
